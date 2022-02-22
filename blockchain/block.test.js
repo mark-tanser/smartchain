19056f07d02273a1eec733c6182784840fb1dfff
Mark Tanser <tanser.mark@gmail.com>
2022-02-22 16:03:21 +0900

    complete s3.17 MineBlock - Block Target Hash

---
+++ blockchain/block.test.js
@@ -0,0 +1,19 @@
+const { it, expect } = require('@jest/globals');
+const Block = require('./block');
+
+describe('Block', () => {
+    describe('calculateBlockTargetHash()', () => {
+        it('calculates the maximukm hash when the last block difficulty is 1', () => {
+            expect(
+                Block.calculateBlockTargetHash({ lastBlock: { blockHeaders: { difficulty: 1 } } })
+            ).toEqual('f'.repeat(64));
+        });
+
+        it('calculates a low hash value when the last block difficulty is high', () => {
+            expect(
+                Block.calculateBlockTargetHash({ lastBlock: { blockHeaders: { difficulty: 500 } } }) < '1'
+            ).toBe(true);
+        })
+
+    })
+})
