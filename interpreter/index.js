d24c823ebb226010ffd5c1fdf01fd1e696b7aa24
Mark Tanser <tanser.mark@gmail.com>
2022-02-22 11:50:09 +0900

    complete s2.8 Jumps

--- interpreter/index.js
+++ interpreter/index.js
@@ -9,6 +9,8 @@ const GT = 'GT'; // greater than
 const EQ = 'EQ'; // equal to
 const AND = 'AND';
 const OR = 'OR';
+const JUMP = 'JUMP';
+const JUMPI = 'JUMPI'; // jump if
 
 class Interpreter {
     constructor() {
@@ -17,6 +19,13 @@ class Interpreter {
         stack: [],
         code: []
     };
+}
+
+jump() {
+    const destination = this.state.stack.pop();
+
+    this.state.programCounter = destination;
+    this.state.programCounter --;
 }
 
 runCode(code) {
@@ -59,6 +68,15 @@ runCode(code) {
                     if (opCode == OR) result = a || b;
 
                     this.state.stack.push(result);
+                    break;
+                case JUMP:
+                    this.jump();
+                    break
+                case JUMPI:
+                    const condition = this.state.stack.pop();
+                    if (condition === 1) {
+                        this.jump();
+                    }
                     break;
 
 
@@ -112,3 +130,11 @@ console.log('Result of 0 AND 1', result);
 code = [PUSH, 1, PUSH, 0, OR, STOP];
 result = new Interpreter().runCode(code);
 console.log('Result of 0 OR 1', result);
+
+code = [PUSH, 6, JUMP, PUSH, 0, JUMP, PUSH, 'jump successful', STOP];
+result = new Interpreter().runCode(code);
+console.log('Result of JUMP:', result);
+
+code = [PUSH, 8, PUSH, 1, JUMPI, PUSH, 0, JUMP, PUSH, 'jump successful', STOP];
+result = new Interpreter().runCode(code);
+console.log('Result of JUMPI:', result);
