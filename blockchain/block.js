acf16c4631e201a42295ab750710f549d275ba9f
Mark Tanser <tanser.mark@gmail.com>
2022-02-22 16:39:52 +0900

    complete s3.18 MineBlock - Under Target Hash

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,6 +1,9 @@
 const { GENESIS_DATA } = require('../config');
+const { keccakHash } = require('../util');
+
 const HASH_LENGTH = 64;
 const MAX_HASH_VALUE = parseInt('f'.repeat(HASH_LENGTH), 16);
+const MAX_NONCE_VALUE = 2 ** 64;
 ;
 class Block {
     constructor({ blockHeaders }) {
@@ -19,7 +22,32 @@ class Block {
     
 
     static mineBlock({ lastBlock, beneficiary }) {
+        const target = Block.calculateBlockTargetHash({ lastBlock });
+        let timestamp, truncatedBlockHeaders, header, nonce, underTargetHash;
 
+        do {
+            timestamp = Date.now();
+            truncatedBlockHeaders = {
+                parentHash: keccakHash(lastBlock.blockHeaders),
+                beneficiary,
+                difficulty: lastBlock.blockHeaders.difficulty + 1,
+                number: lastBlock.blockHeaders.number + 1,
+                timestamp
+            };
+            header = keccakHash(truncatedBlockHeaders);
+            nonce = Math.floor(Math.random() * MAX_NONCE_VALUE);
+    
+            underTargetHash = keccakHash(header + nonce);
+
+            console.log('underTargetHash', underTargetHash);
+            console.log('target', target);
+
+        } while (underTargetHash > target);
+
+        return new this({
+            blockHeaders: { ...truncatedBlockHeaders, nonce }
+        });
+        
     }
 
     static genesis() {
@@ -28,3 +56,10 @@ class Block {
 }
 
 module.exports = Block;
+
+const block = Block.mineBlock({
+    lastBlock: Block.genesis(),
+    beneficiary: 'foo'
+});
+
+console.log('block', block);
